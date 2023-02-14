5a2d8b038396078689a9426a350d1701ce74bc73
SehrishHussain <sehrishaslam1@hotmail.com>
2023-02-14 12:54:57 +0500

    Checking user sum

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 while True:
     try:
         level = int(input("Choose a level between 1 and 3:"))
@@ -14,6 +16,26 @@ while True:
         break
 
 if 1 <= level <= 3:
-    print("success")
+    print('You have chosen level', level)
 else:
-    print("not success")
+    pass
+if level == 1:
+    a, b = random.sample(range(0, 9), 2)
+elif level == 2:
+    a, b = random.sample(range(10, 99), 2)
+elif level == 3:
+    a, b = random.sample(range(100, 999), 2)
+
+sum1 = a + b
+
+for i in range(1, 11):
+    print('Round', i)
+    x = 3
+    user_sum = int(input(f'What is {a} + {b}?:'))
+    if sum1 == user_sum:
+        print('Correct! Next round.')
+    elif sum1 != user_sum:
+        x = x - 1
+        print(f'Incorrect - you have {x} attempts remaining.')
+
+
